28fc10f07eb16d9e4e9018eb54db2cfa9b3d43aa
mateusz.sokolski <mateusz.sokolski@britenet.com.pl>
2019-12-17 11:21:34 +0100

    raw nodes

--- src/main/scala/pl/com/britenet/nodes/Main.scala
+++ src/main/scala/pl/com/britenet/nodes/Main.scala
@@ -6,6 +6,8 @@ import scala.jdk.CollectionConverters._
 
 case class Node(id: Int, name: String, nodes: List[Node])
 
+case class RawNode(id: Int, name: String, row: Int, col: Int)
+
 object Main {
   def main(args: Array[String]): Unit = {
     val source = getClass.getClassLoader.getResourceAsStream("test1.xlsx")
@@ -16,6 +18,23 @@ object Main {
     val isRowANode = (row: Row) => row.getCell(3).getCellType == CellType.NUMERIC
     val filteredRows = rows.filter(isRowANode)
 
-//    filteredRows.foreach(row => println(row.getHeight))
+    val rawNodes = filteredRows.zipWithIndex.map { case (row, index) =>
+      mapRow(row, index)
+    }
+
+    println(rawNodes)
+  }
+
+  def mapRow(row: Row, index: Int): RawNode = {
+    val id = row.getCell(3).getNumericCellValue.intValue
+
+    val cellAndIndex = row.cellIterator().asScala.toList.zipWithIndex.find {
+      case (cell, _) => cell.getCellType == CellType.STRING
+    }.get
+
+    val name = cellAndIndex._1.getStringCellValue
+    val col = cellAndIndex._2
+
+    RawNode(id, name, index, col)
   }
 }
