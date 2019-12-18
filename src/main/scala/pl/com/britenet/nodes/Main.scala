920e8c4f0800b90278bdd8b015323994922ad7b2
mateusz.sokolski <mateusz.sokolski@britenet.com.pl>
2019-12-18 11:34:55 +0100

    wip

--- src/main/scala/pl/com/britenet/nodes/Main.scala
+++ src/main/scala/pl/com/britenet/nodes/Main.scala
@@ -2,10 +2,18 @@ package pl.com.britenet.nodes
 
 import org.apache.poi.ss.usermodel.{CellType, Row, WorkbookFactory}
 
-import scala.annotation.tailrec
 import scala.jdk.CollectionConverters._
 
-case class Node(id: Int, name: String, nodes: List[Node])
+trait Printable {
+  def print(): Unit
+}
+
+case class Node(id: Int, name: String, nodes: List[Node]) extends Printable {
+  def print(): Unit = {
+    println(id, name)
+    nodes.foreach(node => node.print())
+  }
+}
 
 case class RawNode(id: Int, name: String, row: Int, col: Int)
 
@@ -14,21 +22,58 @@ object Main {
     val source = getClass.getClassLoader.getResourceAsStream("test1.xlsx")
     val workbook = WorkbookFactory.create(source)
 
-    val rows = workbook.getSheetAt(0).iterator().asScala.toList
+    val mapper = new Mapper
 
+    val rawNodes = mapper.mapRawNodes(workbook.getSheetAt(0).iterator().asScala.toList)
+    val families = mapper.createFamily(rawNodes.groupBy(node => node.col)(0), rawNodes)
+
+    println(families)
+  }
+}
+
+class Mapper() {
+
+  def mapRawNodes(rows: List[Row]): List[RawNode] = {
     val isRowANode = (row: Row) => row.getCell(3).getCellType == CellType.NUMERIC
-    val filteredRows = rows.filter(isRowANode)
 
-    val rawNodes = filteredRows.zipWithIndex.map { case (row, index) =>
-      mapRow(row, index)
+    rows.filter(isRowANode).zipWithIndex.map {
+      case (row, index) => mapRow(row, index)
     }
-
-    createTree(rawNodes)
-
-    println(rawNodes)
   }
 
-  def mapRow(row: Row, index: Int): RawNode = {
+
+  def createFamily(parents: List[RawNode], orphanedChildren: List[RawNode]): List[Node] = {
+    parents match {
+      case List() => List[Node]()
+      case List(x) =>
+    List[Node](Node(x.id, x.name, createFamily(assignChildren(x, orphanedChildren), orphanedChildren)))
+      case x :: xs =>
+    Node(x.id, x.name, createFamily(assignChildren(x, orphanedChildren), orphanedChildren)) :: createFamily(xs, orphanedChildren)
+    }
+  }
+
+  private def assignChildren(parent: RawNode, orphanedChildren: List[RawNode]): List[RawNode] = {
+    val nextParent = orphanedChildren.find(x => x.col == parent.col && x.row > parent.row)
+
+    if (nextParent.isEmpty)
+    orphanedChildren.filter(child => isNextGen(parent, child) && isAfter(parent, child))
+    else
+    orphanedChildren.filter(child => isNextGen(parent, child) && isBefore(nextParent.get, child) && isAfter(parent, child))
+  }
+
+  private def isNextGen(parent:RawNode, child:RawNode): Boolean = {
+    parent.col + 1 == child.col
+  }
+
+  private def isBefore(parent:RawNode, child:RawNode): Boolean = {
+    parent.row > child.row
+  }
+
+  private def isAfter(parent:RawNode, child:RawNode): Boolean = {
+    parent.row < child.row
+  }
+
+  private def mapRow(row: Row, index: Int): RawNode = {
     val id = row.getCell(3).getNumericCellValue.intValue
 
     val cellAndIndex = row.cellIterator().asScala.toList.zipWithIndex.find {
@@ -39,28 +84,5 @@ object Main {
     val col = cellAndIndex._2
 
     RawNode(id, name, index, col)
-  }
-
-  def createTree(list: List[RawNode]): List[Node] = {
-    val byCol = list.groupBy(node => node.col)
-
-    val value = addChildren(byCol(0), byCol(1))
-
-    List[Node]()
-  }
-
-  def addChildren(parents: List[RawNode], children: List[RawNode]): List[Node] = {
-    parents match {
-      case List(x) =>
-        val nodes = children.filter(node => node.row > x.row)
-        List[Node](Node(x.id, x.name, nodes.map(node => mapRawNode(node))))
-      case x :: xs =>
-        val nodes = children.filter(node => node.row > x.row && node.row < xs.head.row)
-        Node(x.id, x.name, nodes.map(node => mapRawNode(node))) :: addChildren(xs, children)
-    }
-  }
-
-  def mapRawNode(rawNode: RawNode): Node = {
-    Node(rawNode.id, rawNode.name, List[Node]())
   }
 }
